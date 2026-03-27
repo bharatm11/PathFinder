fdb211c9a1df99dfa4e6e2014cb3039b7c3d5696
Bharat Mathur <bharat.mathur@hotmail.com>
2026-03-27 17:38:13 -0500

    test map

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-#include "CellType.hpp"
-#include "Position.hpp"
+#include "Map.hpp"
+
+namespace
+{
+char cellToChar(CellType cell)
+{
+    switch (cell)
+    {
+    case CellType::Start:
+        return 'S';
+    case CellType::Target:
+        return 'T';
+    case CellType::Elevated:
+        return '#';
+    case CellType::Open:
+    default:
+        return '.';
+    }
+}
+
+void printMap(const Map &map)
+{
+    for (int r = 0; r < map.numRows(); ++r)
+    {
+        for (int c = 0; c < map.numCols(); ++c)
+        {
+            std::cout << cellToChar(map.getCellType({r, c})) << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+} // namespace
+
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    Map map;
+    map.generateRandom(32, 32);
+    printMap(map);
+
     return 0;
 }
